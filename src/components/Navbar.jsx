a41cda804623b749df8346f01d6dfc7907b12c1b
Sirivalli <ladellasirivalli@gmail.com>
2025-12-10 17:07:04 +0530

    removed donate button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,9 +22,7 @@ export default function Navbar() {
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
 
-        <button className="bg-primary text-white px-4 py-2 rounded-md hover:bg-secondary">
-          Donate
-        </button>
+       
       </ul>
     </nav>
   );
